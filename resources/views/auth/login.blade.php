cf40a3e98f8b69f2d82b9ad3d536c644386cbe92
ashu mehra <Ashumehra842@github.com>
2020-06-08 20:40:01 +0530

    login with Google Api

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,10 +54,10 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                    {{ __('Login') }}  
                                 </button>
-                                <span>OR</span>
-                                <a href="{{ url('login/google') }}" class="btn btn-success">
+                                <span>OR</span> 
+                                <a href="{{ url('login/google') }}" class="btn btn-info">
                                     {{ __('Login with Google') }}
                                 </a>
 
